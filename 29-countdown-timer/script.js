d2147d525909b572ed0f45d6311fcc7d6c6d5058
rnegi0598 <rnegi0598@gmail.com>
2023-04-27 20:28:19 +0530

    updated project structure

--- 29-countdown-timer/script.js
+++ 29-countdown-timer/script.js
@@ -3,31 +3,38 @@ const display=document.body.querySelector('.time-display');
 const buttons=Array.from(document.body.querySelectorAll('button[data-time]'));
 const input=document.body.querySelector('input');
 const msg=document.body.querySelector('.msg b');
+//in input field on pressing enter key the countdown timer will start
 input.addEventListener('keyup',(e)=>{
     if(e.key==='Enter'){
         if(!Number(e.target.value))return ;
         countdown(Number(e.target.value)*60);
+        e.target.value='';
+        e.target.blur(); //moves cursor out of the input field
     }
 })
+//oin clicking the respective button the respective countdown timer will start
 buttons.forEach((button)=>{
     button.addEventListener('click',(e)=>{
         countdown(parseInt(e.target.dataset.time));
     })
 })
+//we keep timerId to clear setInterval before calling on it another time
 let timerId=-1;
 
+//given time in sec start the timer
 function countdown(time){
-    //time in sec
     
     clearInterval(timerId);
-    const now=Date.now();
+
+    const now=Date.now(); //in milli sec
     const then =now+time*1000;
-    // 
+    //time at which timer will end
     const endTime=new Date(then);
+    // for msg calulate hours and min at which the timer will finish 
     const hours=endTime.getHours();
     const min=endTime.getMinutes();
     msg.innerHTML=`${hours>12?hours%12:hours}: ${min<10?'0'+min:min} ${hours>12?'pm':'am'}`
-    // 
+    //as callback function to setInterval will load after 1 sec , we call the function displayTime one time before the setInterval 
     displayTime(time);
     timerId=setInterval(()=>{
         const secLeft=Math.round((then-Date.now())/1000);
@@ -35,14 +42,12 @@ function countdown(time){
             clearInterval(timerId);
             return;
         }
-        //work
+        //display time in counter
         displayTime(secLeft);
-        
-        
     },1000);
 
 }
-
+//given total time in sec display it in counter in min and secs
 function displayTime(secLeft){
     const min=formatTime(Math.floor(secLeft/60));
     const sec=formatTime(secLeft%60);
@@ -50,6 +55,7 @@ function displayTime(secLeft){
             ${min}:${sec}
         `
 }
-function formatTime(time){
-    return time<10? `0${time}`:time;
+//given a digit , if it's single digit 0 is appended else nothing is done 
+function formatTime(digit){
+    return digit<10? `0${digit}`:digit;
 }
--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# JS30Projects : [A 30 Vanilla JavaScript Project Challenge !](https://github.com/wesbos/JavaScript30) 
+# JS-and-CSS-Projects :  JS and Css projects for practise
 
-## Playlist 
-
-[![JS30Projects playlist](http://img.youtube.com/vi/VuN8qwZoego/0.jpg)](https://youtube.com/playlist?list=PLu8EoSxDXHP6CGK4YVJhL_VWetA865GOH)
   
 - [x] Make a JS drumKit
 - [x] JS analog clock
@@ -11,10 +8,8 @@
 - [x] Image gallery using flex
 - [x] fetch() API
 - [x] Array some,every,find ,spread 
-- [ ] HTML5 canvas
 - [x] Dev tools
 - [x] JS checkbox
-- [ ] Custom HTML5 videoplayer
 - [x] JS konami code
 - [x] JS slide in on scroll
 - [x] JS :Reference vs copy
@@ -22,13 +17,7 @@
 - [x] css text shadow on mouse move effect
 - [x] Sorting band names without articles
 - [x] How JS array reduce works
-- [ ] Unreal webcam fun with getUserMedia() and HTML5 Canvas
-- [ ] JS speech recognition
-- [ ] JS geolocation based speedometer and compass
-- [ ] JS Exercise
-- [ ] JS text-to-speech
 - [x] JS sticky nav
 - [x] JS interface challenge
-- [ ] Build experimental video speed controller UI
 - [x] JS countdown timer
-- [ ] Make a whack a mole game
+
