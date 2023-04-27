fe4b4fffe8c306d0b4d6b5a6a9b2fb56aa4a6f8c
rnegi0598 <rnegi0598@gmail.com>
2023-04-28 00:24:03 +0530

    added comment

---
+++ 14-mousemove-shadow/README.md
@@ -0,0 +1,2 @@
+## text-shadow property
+text-shadow: h-shadow v-shadow blur-radius color
--- 14-mousemove-shadow/script.js
+++ 14-mousemove-shadow/script.js
@@ -4,7 +4,7 @@ const walk=200;
 hero.addEventListener('mousemove',(e)=>{
     const {x,y}=e;
     const {offsetWidth:width,offsetHeight:height}=hero;
-
+    //x/width * walk gives distance relative to walk  and we subtract walk/2 to get distance from center to left as positive and right as negative.
     const xWalk=Math.round((x/width*walk)-walk/2);
     const yWalk=Math.round((y/height*walk)-walk/2);
    
